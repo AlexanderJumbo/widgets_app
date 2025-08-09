414fdd39fcca6c711f55ba242f97c94e085d8da7
AlexanderJumbo <geossephyjumboalava@gmail.com>
2025-08-08 21:47:08 -0500

    Rutas con nombre

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/screen/cards/cards_screen.dart';
 
 
 
 class HomeScreen extends StatelessWidget {
+
+  static const String name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
@@ -54,14 +58,16 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),  
       subtitle: Text(menuItem.subTitle),
       onTap: (){
-          //* Primer forma
+          //* Primer forma - flutter
           // Navigator.of(context).push(
           //   MaterialPageRoute(builder: (context) => const ButtonsScreen()),
           // );
-          //* Segunda forma
+          //* Segunda forma - flutter
           // Navigator.pushNamed(context, menuItem.link);
-          //* 3era forma
-          context.push(menuItem.link);
+          //* 3era forma - goRouter
+          // context.push(menuItem.link);
+          //* 4ta forma - goRouter pero para rutas con nombre
+          context.pushNamed(CardsScreen.name);
       },
     );
   }

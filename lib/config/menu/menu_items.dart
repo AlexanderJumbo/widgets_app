916865b17860613064c1d11bada059a718456499
AlexanderJumbo <geossephyjumboalava@gmail.com>
2025-08-15 23:18:48 -0500

    UI-Screen end

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -41,5 +41,10 @@ const appMenuItems = <MenuItem>[
       title: 'Animated container',
       subTitle: 'Stateful widget animado',
       link: '/animated',
-      icon: Icons.check_box_outline_blank_rounded)
+      icon: Icons.check_box_outline_blank_rounded),
+  MenuItem(
+      title: 'UI Controls + Tiles',
+      subTitle: 'Serie de controles de Flutter',
+      link: '/ui-controls',
+      icon: Icons.car_rental_outlined)
 ];
--- lib/presentation/screen/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screen/ui_controls/ui_controls_screen.dart
@@ -7,6 +7,112 @@ class UiControlsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UI Controls'),
+      ),
+      body: _UIControlsView(),
+    );
+  }
+}
+
+class _UIControlsView extends StatefulWidget {
+  const _UIControlsView();
+
+  @override
+  State<_UIControlsView> createState() => _UIControlsViewState();
+}
+
+enum Transportation {car, plane, boat, submarine}
+
+class _UIControlsViewState extends State<_UIControlsView> {
+
+  bool isDeveloper = true;
+  Transportation selectedTransportation = Transportation.car;
+
+  bool wantsBreakfast = false;
+  bool wantsLunch = false;
+  bool wantsDinner = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(), //*quita el mov de rebote
+      children: [
+        SwitchListTile(
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Controles adicionales'),
+          value: isDeveloper, 
+          onChanged: (value) => setState(() {
+            isDeveloper = !isDeveloper;   
+          }),
+        ),
+
+        ExpansionTile(
+          title: const Text('Vehículo de transporte'),
+          subtitle: Text('$selectedTransportation'),
+          children: [
+            RadioListTile(
+              title: const Text('By Car'),
+              subtitle: const Text('Viajar por carro'),
+              value: Transportation.car,
+              groupValue: selectedTransportation,
+              onChanged: (value) => setState(() {
+                selectedTransportation = Transportation.car;
+              }),
+            ),
+            RadioListTile(
+              title: const Text('By Boat'),
+              subtitle: const Text('Viajar por barco'),
+              value: Transportation.boat,
+              groupValue: selectedTransportation,
+              onChanged: (value) => setState(() {
+                selectedTransportation = Transportation.boat;
+              }),
+            ),
+            RadioListTile(
+              title: const Text('By Plane'),
+              subtitle: const Text('Viajar por plane'),
+              value: Transportation.plane,
+              groupValue: selectedTransportation,
+              onChanged: (value) => setState(() {
+                selectedTransportation = Transportation.plane;
+              }),
+            ),
+            RadioListTile(
+              title: const Text('By Submarine'),
+              subtitle: const Text('Viajar por submarino'),
+              value: Transportation.submarine,
+              groupValue: selectedTransportation,
+              onChanged: (value) => setState(() {
+                selectedTransportation = Transportation.submarine;
+              }),
+            ),
+          ],
+        ),
+        CheckboxListTile(
+          title: Text('¿Desayuno?'),
+          value: wantsBreakfast, 
+          onChanged: (value) => setState(() {
+            wantsBreakfast = !wantsBreakfast;
+          }),
+        ),
+        CheckboxListTile(
+          title: Text('Almuerzo?'),
+          value: wantsLunch, 
+          onChanged: (value) => setState(() {
+            wantsLunch = !wantsLunch;
+          }),
+        ),
+        CheckboxListTile(
+          title: Text('Cena?'),
+          value: wantsDinner, 
+          onChanged: (value) => setState(() {
+            wantsDinner = !wantsDinner;
+          }),
+        )
+        
+      ],
+    );
   }
 }
